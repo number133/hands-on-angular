9c5f5ce3f1dd39008bb6ea1ca4c8a2cabfceeb2d
Abylay.Sabirgaliyev <Abylay.Sabirgaliyev@jazzsoft.kz>
2016-01-21 17:19:28 +0600

    NavigationController test of isActive method

--- app/scripts/controllers/navigation.js
+++ app/scripts/controllers/navigation.js
@@ -3,6 +3,7 @@
 angular.module('swFrontApp')
 	.controller('NavigationController', function($scope, $location){
 	$scope.isActive = function(path){
-		return path === $location.path();
+		var currentPath = $location.path().split('/')[1].split('?')[0];
+		return currentPath === path.split('/')[1];
 	}
 });
